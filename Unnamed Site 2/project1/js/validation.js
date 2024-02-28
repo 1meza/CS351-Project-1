8086469e8f28aaabdef2ea0e6768a798f706a837
Meza, Isaiah <72046367+1meza@users.noreply.github.com>
2024-02-27 21:45:19 -0800

    Add files via upload

--- Unnamed Site 2/project1/js/validation.js
+++ Unnamed Site 2/project1/js/validation.js
@@ -3,35 +3,36 @@
 (() => {
   'use strict'
 
-  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  // fetch all forms we want to apply custom Bootstrap validation styles to
   const forms = document.querySelectorAll('.needs-validation');
 
-  // Loop over them and prevent submission
+  // loop over them and prevent submission
   Array.from(forms).forEach(form => {
     form.addEventListener('submit', event => {
-      let isValid = true;
+		// fetch input password from create account
+      const passwordInput = form.querySelector('#inputPassword4'); 
+		// fetch the feedback for the user password
+      const passwordFeedback = form.querySelector('#inputPassword4 + .invalid-feedback');
+      const MIN_LENGTH_PASS = 6; // Minimum password length
+
+      if (passwordInput && passwordInput.value.length < MIN_LENGTH_PASS) {
+        // update the feedback message for password length
+        passwordFeedback.textContent = `Password must be at least 6 characters long.`;
+        passwordInput.setCustomValidity('INVALID');
+
+      } else {
+		  // once successfull set the validity to none and reset default
+        passwordInput.setCustomValidity('');
+      }
+
+
       if (!form.checkValidity()) {
         event.preventDefault();
         event.stopPropagation();
-        isValid = false;
       }
 
-      const optionalFields = form.querySelectorAll('#inputAddress, #inputAddress2 ,#inputCity, #inputState, #inputZip, #inputPhone');
-      optionalFields.forEach(field => {
-        if (field.value.trim() !== '') {
-          if (!field.checkValidity()) {
-            field.classList.add('is-invalid');
-            isValid = false;
-          } else {
-            field.classList.remove('is-invalid');
-          }
-        } else {
-          // If the field is empty, remove any invalid class that might be set
-          field.classList.remove('is-invalid');
-        }
-      });
-
       form.classList.add('was-validated');
+
     }, false);
   });
 })();
